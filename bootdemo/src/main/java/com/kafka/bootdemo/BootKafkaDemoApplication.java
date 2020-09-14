7d2d13b2c4e4c0210e99aacbd2958b55c4f95f92
snag <somjitnag192@gmail.com>
2020-09-14 13:55:04 +0530

    initial commit

---
+++ bootdemo/src/main/java/com/kafka/bootdemo/BootKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.bootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootKafkaDemoApplication.class, args);
+	}
+
+}
