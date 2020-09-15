7abbb8099315a0187b7f3e1d78088546c83552b7
snag <somjitnag192@gmail.com>
2020-09-15 21:41:13 +0530

    initial commit

---
+++ bootdemo/src/main/java/com/kafka/bootdemo/resources/UserResource.java
@@ -0,0 +1,14 @@
+package com.kafka.bootdemo.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka/v1/")
+public class UserResource {
+//
+//    @GetMapping("/publish/message")
+//    public String post()
+
+}
