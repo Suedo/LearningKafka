b729031e310791b23eaa898e2cd25b98a10bb448
snag <somjitnag192@gmail.com>
2020-10-10 08:49:02 +0530

    basic consumer works

---
+++ bootdemo/src/main/java/com/kafka/bootdemo/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,16 @@
+package com.kafka.bootdemo.consumer;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class LibraryEventsConsumer {
+
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) {
+        log.info("Consumer Record: {}", consumerRecord);
+    }
+}
