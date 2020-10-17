6cbd7b1c7f43b4fb3456f9f78ebe32b20281f14b
snag <somjitnag192@gmail.com>
2020-10-17 19:46:28 +0530

    producer error retries: setup recreating the setup where a producer fails because #brokers < #ISRs mentioned in topic

--- springbootkafka/notes/course_commands.sh
+++ springbootkafka/notes/course_commands.sh
@@ -3,6 +3,7 @@ docker-compose exec broker bash
 
 # then issue command for creating a topic
 kafka-topics --create --topic library-events --bootstrap-server broker:9092 --replication-factor 3 --partitions 3
+kafka-topics --create --topic library-events --bootstrap-server broker:9092 --replication-factor 3 --partitions 3 --config min.insync.replicas=2
 
 
 # have a console consumer up before you hit a producer endpoint. this will help us visually see the output in the consumer
